42eac008e8d673417e4329ceee4f03613f69129f
cbasile <basilechiara@libero.it>
2023-02-09 17:48:49 +0100

    first commit

---
+++ templates/toWrapUpAmplitudes.sh
@@ -0,0 +1,30 @@
+#
+# To write on file the amolitudes to fit
+#
+
+### 25 GeV
+
+#python templates/plot_amplitudes.py -r 15183 -e 25 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 10 --maxamp 700 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+### 50 GeV
+
+#python templates/plot_amplitudes.py -r 15146 -e 50 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 500 --maxamp 2000 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+
+### 75 GeV
+
+python templates/plot_amplitudes.py -r 15199 -e 75 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 1000 --maxamp 2500 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+### 100 GeV
+
+#python templates/plot_amplitudes.py -r 15153 -e 100 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 1500 --maxamp 3000 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+### 125 GeV
+
+python templates/plot_amplitudes.py -r 15190 -e 125 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 2000 --maxamp 3500 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+### 150 GeV
+
+#python templates/plot_amplitudes.py -r 15158 -e 150 --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0 --minamp 2000 --maxamp 4000 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ 
+
+
---
+++ templates/toWrapUpTemplates.sh
@@ -0,0 +1,27 @@
+#
+# To put all templates in separated files
+#
+#
+### 25 GeV
+
+python templates/plot_templates_energy.py -r 15183 -e 25 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
+
+### 50 GeV
+
+python templates/plot_templates_energy.py -r 15146 -e 50 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
+
+### 75 GeV
+
+#python templates/plot_templates_energy.py -r 15199 -e 75 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
+
+### 100 GeV
+
+#python templates/plot_templates_energy.py -r 15153 -e 100 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
+
+### 125 GeV
+
+#python templates/plot_templates_energy.py -r 15190 -e 125 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
+
+### 150 GeV
+
+python templates/plot_templates_energy.py -r 15158 -e 150 --out /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results/ --path /eos/user/c/cbasile/ECAL_TB2021/ntuples_templates/ntuples_v1/
