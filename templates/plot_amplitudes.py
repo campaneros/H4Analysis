00c0ed3cc15f0910bc3f647ecc54a7e02a91bbc9
cbasile <basilechiara@libero.it>
2023-02-22 21:44:29 +0100

    correction for pile-up

--- templates/plot_amplitudes.py
+++ templates/plot_amplitudes.py
@@ -12,6 +12,7 @@ parser.add_argument('--path' , default = '/eos/user/c/cbasile/ECAL_TB2021/ntuple
 parser.add_argument('--out' , default = '/eos/user/c/cbasile/ECAL_TB2021/ntuples_templates_v0/plot_results')
 parser.add_argument('--minamp' , type = int, default = 1500)
 parser.add_argument('--maxamp' , type = int, default = 3000)
+parser.add_argument('--maxchi' , type = int, default = 10000)
 parser.add_argument('--cx' , type = float, default = 0.0)
 parser.add_argument('--cy' , type = float, default = -5.0)
 
@@ -25,6 +26,9 @@ Amax = args.maxamp
 
 ROOT.gStyle.SetOptStat(0)
 amps = ROOT.TH1F(f'amps_{E}', "", 100, Amin, Amax)
+amps.SetTitle(f'{E} GeV')
+amps.GetXaxis().SetTitle("ADC counts")
+amps.GetYaxis().SetTitle(f'entries/{amps.GetXaxis().GetBinWidth(10)} ADC')
 
 h4 = ROOT.TChain('h4')
 h4.Add(f'{path}/{run}/*.root')
@@ -33,8 +37,28 @@ hodo1_X.Add(f'{path}/{run}/*.root')
 hodo1_Y = ROOT.TChain('h1Y')
 hodo1_Y.Add(f'{path}/{run}/*.root')
 
-h4.Draw(f'fit_ampl[C2]>>amps_{E}', f'fit_ampl[C2] > {Amin} && fit_ampl[C2]<{Amax} && fit_ampl[MCP1]>120 && fabs(h1X.clusters.X_- {args.cx}) && fabs(h1Y.clusters.Y_- {args.cy})', 'goff')
+nentries = h4.GetEntries()
+Nsaturated = 0
+print(f'... {nentries} events to be processed')
+for e in range(nentries):
+    #if (e > 10000): break
+    h4.GetEntry(e)
+    hodo1_X.GetEntry(e)
+    hodo1_Y.GetEntry(e)
+    n = h4.Draw('WF_val:WF_time>>h','WF_ch==C2', 'goff', 1, e)
+    #print(f' nWF = {n}')
+    if( n > 90 ): 
+        Nsaturated += 1
+        continue
 
+    n = h4.Draw(f'fit_ampl[C2]', f'trg==PHYS&&amp_max[C2] > {Amin} && amp_max[C2]<{Amax} && fit_ampl[MCP1]>150 && fabs(h1X.clusters.X_- {args.cx}) && fabs(h1Y.clusters.Y_- {args.cy})', 'goff',1,e)
+    if(n < 1): continue 
+    fit_ampl = h4.GetVal(0)[0]
+    #print(f' N = {n} Afit = {fit_ampl}')
+    amps.Fill(fit_ampl)
+
+Nsaturated = float(Nsaturated)/nentries
+print(f' = fraction of saturated events {Nsaturated}')
 outFile = ROOT.TFile(f'{out}/amp_{E}.root', "RECREATE")
 print(f'... OUTPUT saved in {out}/amp_{E}.root')
 amps.Write()
