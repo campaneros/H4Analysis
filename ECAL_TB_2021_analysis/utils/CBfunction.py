4155c1a460cfebc27fc2de3fccf69f16cf671f2f
cbasile <basilechiara@libero.it>
2023-04-27 17:03:38 +0200

    fixed selection

--- ECAL_TB_2021_analysis/utils/CBfunction.py
+++ ECAL_TB_2021_analysis/utils/CBfunction.py
@@ -12,15 +12,16 @@ class CBfunction:
     doubleSidedCB = False
     gaussian = False
     
-    nbins =480 
+    fitchi_max =5000 
+    nbins =600
     xmin = 0
     xmax = 12000
     xaxis_scale = 0.2
     a_initial = 0.5
-    n_initial = 7
-    a2_initial = 0.5
-    n2_initial = 7    
-    s_initial = 60
+    n_initial = 5 
+    a2_initial = .5
+    n2_initial = 50 
+    s_initial = 50
 
     def __init__(self,data):
         self.data = data ## data should be TChain already
@@ -46,7 +47,9 @@ class CBfunction:
         
     def set_selection(self):
         #self.selection = "fit_ampl[MCP1]>120 && fabs(X-(%.2f))<%.2f && fabs(Y-(%.2f))<%.2f"%(self.xcenter,self.window,self.ycenter,self.window)
-        self.selection = f' amp_max[C2] > {self.Amin} && amp_max[C2]< {self.Amax} && fit_ampl[MCP1]>120 && fabs(h1X.clusters.X_- {self.xcenter}) && fabs(h1Y.clusters.Y_- {self.ycenter})'
+        #self.selection = f'trg==PHYS && fit_chi2[{self.crystal}] < {self.fitchi_max} && fit_ampl[MCP1]>200&& fabs(h1X.clusters.X_- {self.xcenter}) < {self.window} && fabs(h1Y.clusters.Y_- {self.ycenter})  < {self.window} && fabs(h1X.clusters.X_ - h2X.clusters.X_ - 1) < 1 && fabs(h1Y.clusters.Y_ - h2Y.clusters.Y_ + 1) < 1'
+        self.selection = f'trg==PHYS && evt_flag && fit_chi2[{self.crystal}] < {self.fitchi_max} && fit_ampl[MCP1]>200 && fabs(X - {self.xcenter}) < {self.window} && fabs(Y- {self.ycenter}) < {self.window}'
+        print(f' --> selection : {self.selection}')
 
     def prepare_sumhistogram(self,dict_crystals_calibration,matrix):
         self.set_selection()
@@ -55,7 +58,9 @@ class CBfunction:
         draw_function = '('
         for enum,cryst in enumerate(matrix):
             #draw_function +='fit_ampl[%s]*%.4f'%(cryst,dict_crystals_calibration[cryst])
-            draw_function +='((fit_ampl[%s] > 0) ? fit_ampl[%s] : amp_max[%s])*%.4f'%(cryst,cryst,cryst,dict_crystals_calibration[cryst])
+            #draw_function +='((fit_ampl[%s] > 0) ? fit_ampl[%s] : amp_max[%s])*%.4f'%(cryst,cryst,cryst,dict_crystals_calibration[cryst])
+            # SENZA INTERCALIB !
+            draw_function +='((fit_ampl[%s] > 0) ? fit_ampl[%s] : amp_max[%s])*%.4f'%(cryst,cryst,cryst,1.)
             if enum!=len(matrix)-1 : draw_function+='+'
           #  else : draw_function+=")*%.4f>>ampl_%s_%s"%(dict_crystals_calibration['conversion_factor'],self.crystal,self.energy)
             else : draw_function+=")>>ampl_%s_%s"%(self.crystal,self.energy)
@@ -90,13 +95,13 @@ class CBfunction:
         #round_energy = round(float(self.energy),-1)
         #if round_energy ==240 : round_energy = 250
         round_energy = int(self.energy)
-        #self.x = RooRealVar("signal_%s_%dGeV"%(self.crystal,round_energy),"signal_%s_%dGeV"%(self.crystal,round_energy),max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
-        self.x = RooRealVar("signal_%s_%dGeV"%(self.crystal,round_energy),"signal_%s_%dGeV"%(self.crystal,round_energy),self.Amin, self.Amax)
+        self.x = RooRealVar("signal_%s_%dGeV"%(self.crystal,round_energy),"signal_%s_%dGeV"%(self.crystal,round_energy),max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
+        #self.x = RooRealVar("signal_%s_%dGeV"%(self.crystal,round_energy),"signal_%s_%dGeV"%(self.crystal,round_energy),self.Amin, self.Amax)
         self.roohist = RooDataHist("roohist_fit_%s_%s"%(self.crystal,self.energy),"roohist_fit_%s_%s"%(self.crystal,self.energy),RooArgList(self.x),self.hist)
         self.m = RooRealVar("mean_%s_%s"%(self.crystal,self.energy),"mean_%s_%s"%(self.crystal,self.energy),self.peak_position,max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
-        self.s = RooRealVar("sigma_%s_%s"%(self.crystal,self.energy),"sigma_%s_%s"%(self.crystal,self.energy),self.s_initial,10,500) #500.
-        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),self.a_initial,-10.,10)
-        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),self.n_initial,0.,500)
+        self.s = RooRealVar("sigma_%s_%s"%(self.crystal,self.energy),"sigma_%s_%s"%(self.crystal,self.energy),self.s_initial,5,250) #500.
+        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),self.a_initial,0.001,2)
+        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),self.n_initial,0.01,20)
         self.sig = RooCBShape("signal_%s_%s"%(self.crystal,self.energy),"signal_%s_%s"%(self.crystal,self.energy),self.x,self.m,self.s,self.a,self.n)
         
     def Gausintialization(self):
@@ -112,15 +117,18 @@ class CBfunction:
 
         
     def CB2intialization(self):
-        round_energy = round(float(self.energy),-1)
+        #round_energy = round(float(self.energy),-1)
+        round_energy = int(self.energy)
+
         self.x = RooRealVar("signal_%s_%dGeV"%(self.crystal,round_energy),"signal_%s_%dGeV"%(self.crystal,round_energy),max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
+
         self.roohist = RooDataHist("roohist_fit_%s_%s"%(self.crystal,self.energy),"roohist_fit_%s_%s"%(self.crystal,self.energy),RooArgList(self.x),self.hist)
         self.m = RooRealVar("mean_%s_%s"%(self.crystal,self.energy),"mean_%s_%s"%(self.crystal,self.energy),self.peak_position,max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
-        self.s = RooRealVar("sigma_%s_%s"%(self.crystal,self.energy),"sigma_%s_%s"%(self.crystal,self.energy),self.s_initial,10,500)
-        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),self.a_initial,-10.,10)
-        self.a2 = RooRealVar("alpha2_%s_%s"%(self.crystal,self.energy),"alpha2_%s_%s"%(self.crystal,self.energy),self.a2_initial,-10.,10)
-        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),self.n_initial,0.,100)
-        self.n2 = RooRealVar("exp2_%s_%s"%(self.crystal,self.energy),"exp2_%s_%s"%(self.crystal,self.energy),self.n2_initial,0.,30)
+        self.s = RooRealVar("sigma_%s_%s"%(self.crystal,self.energy),"sigma_%s_%s"%(self.crystal,self.energy),self.s_initial,0.,50)
+        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),self.a_initial,0.001,10)
+        self.a2 = RooRealVar("alpha2_%s_%s"%(self.crystal,self.energy),"alpha2_%s_%s"%(self.crystal,self.energy),self.a2_initial,0.001,5)
+        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),self.n_initial,5.,150)
+        self.n2 = RooRealVar("exp2_%s_%s"%(self.crystal,self.energy),"exp2_%s_%s"%(self.crystal,self.energy),self.n2_initial,10.,500)
         self.sig = ROOT.My_double_CB("signal_%s_%s"%(self.crystal,self.energy),"signal_%s_%s"%(self.crystal,self.energy),self.x,self.m,self.s,self.a,self.n,self.a2,self.n2)        
         
 
@@ -142,7 +150,7 @@ class CBfunction:
 
 
     def plot(self):
-        self.frame = self.x.frame()
+        self.frame = self.x.frame(RooFit.Name ("xframe"), RooFit.Title("E = %s GeV crystal %s"%(self.energy, self.crystal)))
         self.roohist.plotOn(self.frame,RooFit.Name("roohist_chi2_%s_%s"%(self.crystal,self.energy)))
         self.sig.plotOn(self.frame,RooFit.Name("signal_chi2_%s_%s"%(self.crystal,self.energy)))
         ndf = 4 
@@ -150,7 +158,7 @@ class CBfunction:
         self.sig.paramOn(self.frame,RooFit.Layout(0.70,0.99,0.8))
         self.frame.getAttText().SetTextSize(0.02)
 
-        txt_chi2 = ROOT.TText(self.peak_position*(1.0-2*self.xaxis_scale),self.ymax_value*0.95,"Chi2 = %.1f"%self.chi2)
+        txt_chi2 = ROOT.TText(self.peak_position*(1.0-self.xaxis_scale),self.ymax_value*0.95,"Chi2 = %.1f"%self.chi2)
         txt_chi2.SetTextSize(0.04)
         txt_chi2.SetTextColor(ROOT.kRed)
         self.frame.addObject(txt_chi2)
